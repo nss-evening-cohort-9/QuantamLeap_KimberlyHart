bdc787557f1e476655e654c1f1e51e4d96395618
Keith Walker <Keithrwalker418@gmail.com>
2019-09-19 19:09:05 -0500

    added props

--- KimberlyHart/Classes/Hosts/Hosts.cs
+++ KimberlyHart/Classes/Hosts/Hosts.cs
@@ -6,5 +6,20 @@ namespace KimberlyHart.Classes.Hosts
 {
     class Hosts
     {
+        public string Name { get; set; }
+        public int Date { get; set; }
+        public bool BeingUsed { get; set; }
+        public Leapers.Leapers UsedBy { get; set; }
+        public List<Leapers.Leapers> PastUsers { get; set; }
+        public int CostPerDay { get; set; }
+        public Guid Id { get; set; }
+
+        public Hosts(string name, int date)
+        {
+            Name = name;
+            Date = date;
+            BeingUsed = false;
+            Id = new Guid();
+        }
     }
 }
