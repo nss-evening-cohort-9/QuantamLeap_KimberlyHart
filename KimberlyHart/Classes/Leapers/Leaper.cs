9757becbd4adee093e6b904779055f0d75804055
Lwamtekie <ltekie87@yahoo.com>
2019-09-21 13:49:22 -0500

    wip

--- KimberlyHart/Classes/Leapers/Leaper.cs
+++ KimberlyHart/Classes/Leapers/Leaper.cs
@@ -14,7 +14,8 @@ namespace KimberlyHart.Classes.Leapers
         public int Wallet { get; set; }
         public Host HostUsing { get; set; }
 
-        public List<Event> PastEvents = new List<Event>();
+        // public List<Event> PastEvents = new List<Event>();
+        public List<Host> PastHosts = new List<Host>();
 
         public Leaper(string name, int budget)
         {
@@ -22,15 +23,20 @@ namespace KimberlyHart.Classes.Leapers
             Wallet = budget;
         }
 
-        public void Jump(Host whatHost, Event whatEvent)
-        {
-            if (whatHost.Year == whatEvent.Year)
-            {
-                Console.WriteLine($"{Name} You are Jumping into {whatHost.Name}, The year is {whatEvent.Year}");
-                //var budgetCheck = new Wallet.Wallet(this, whatEvent.Year).CheckMoneyToJump(5);
+        //public void Jump(Host whatHost, Event whatEvent)
+        //{
+        // if (whatHost.Year == whatEvent.Year)
+        // {
+        // Console.WriteLine($"{Name} You are Jumping into {whatHost.Name}, The year is {whatEvent.Year}");
+        // var budgetCheck = new Wallet.Wallet(this, whatEvent.Year).CheckMoneyToJump(5);
 
-            }
+        //}
+        public void GetHostInfo(Host hostToShare)
+        {
+            PastHosts.Add(hostToShare);
+            Console.WriteLine($"{ hostToShare.Name} added to HostList in Leaper");
         }
     }
-
 }
+
+
